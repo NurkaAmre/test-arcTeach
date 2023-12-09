303105907b0c827661fa3fb35a83a8fcd8678fb7
NurkaAmre <nurkerey30@gmail.com>
2023-12-09 23:11:45 +0600

    Task list components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/header'
 import Footer from './components/footer'
 import TaskForm from './components/TaskForm'
+import TaskList from './components/Tasklist'
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Header />
       <TaskForm />
+      <TaskList />
       <Footer />
     </div>
   )
--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+const TaskList = () => {
+  const tasks = [
+    {
+      name: 'Example Task 1',
+      description: 'Description for Example Task 1',
+      completed: false,
+      status: 'Pending',
+    },
+    {
+      name: 'Example Task 2',
+      description: 'Description for Example Task 2',
+      completed: true,
+      status: 'Completed',
+    },
+      {
+      name: 'Example Task 3',
+      description: 'Description for Example Task 3',
+      completed: true,
+      status: 'Completed',
+    },
 
-const Tasklist = () => {
+  ];
+
+  const toggleTask = (index: number) => {
+    console.log(`Toggled task at index ${index}`);
+  };
+
   return (
-    <div>Tasklist</div>
-  )
-}
+    <div>
+      <h2>Task List</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onChange={() => toggleTask(index)}
+            />
+            <span className={task.completed ? 'line-through' : ''}>{task.name}</span>
+            <p>{task.description}</p>
+            <span>Status: {task.status}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Tasklist
+export default TaskList;
+
