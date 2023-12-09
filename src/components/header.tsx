e1b81cdd3156f7392f9c5e6a0079831f95f8709e
NurkaAmre <nurkerey30@gmail.com>
2023-12-09 22:50:21 +0600

    Setup fonts

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="bg-radial-header py-4 px-6 flex justify-between items-center">
-      <div className="text-white text-2xl font-bold">ARC Teach</div>
+      <div className="text-cyan-600 text-3xl font-bold font-logo">ARC Teach</div>
       <nav className="space-x-4">
-        <a href="/" className="text-white hover:text-gray-300">Home</a>
-        <a href="/tasks" className="text-white hover:text-gray-300">Tasks</a>
-        <a href="/about" className="text-white hover:text-gray-300">About</a>
+        <a href="/" className="text-white text-lg hover:text-cyan-600">Home</a>
+        <a href="/tasks" className="text-white text-lg hover:text-cyan-600">Tasks</a>
+        <a href="/about" className="text-white text-lg hover:text-cyan-600">About</a>
       </nav>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      // Define custom background colors and gradients
       backgroundImage: {
         'radial-body':
           'radial-gradient(at 0% 100%, hsla(253, 16%, 7%, 1) 0, transparent 50%), radial-gradient(at 50% 0%, hsla(225, 39%, 25%, 1) 0, transparent 50%)',
@@ -13,10 +12,13 @@ export default {
           'radial-gradient(at 0% 0%, hsla(253, 16%, 7%, 1) 0, transparent 50%), radial-gradient(at 50% 100%, hsla(225, 39%, 25%, 1) 0, transparent 50%)',
       },
       backgroundColor: {
-        // Define custom background colors
         'custom-body': '#0F0E13',
         'custom-footer': '#0F0E13',
         'custom-header': '#0F0E13',
+      },
+      fontFamily: {
+        logo: ['Dhurjati', 'sans'],
+        sans: ['Mina', ...defaultTheme.fontFamily.sans],
       },
     },
   },
