3413243e29841dcb044a645d05265c763ded1f38
NurkaAmre <nurkerey30@gmail.com>
2023-12-14 09:17:49 +0600

    Implemented action types

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -9,20 +9,23 @@ interface Task {
 }
 
 const TaskList: React.FC = () => {
-  const initialTasks = [
+  const initialTasks: Task[] = [
     {
+      id: 1,
       name: 'Cooking Beshbarmak',
       description: 'Have to cook beshbarmak for my family',
       completed: false,
       status: 'Completed',
     },
     {
+      id: 2,
       name: 'Pair Programming',
       description: 'Have to do pair programming with my friend',
       completed: true,
       status: 'Pending',
     },
     {
+      id: 3,
       name: 'Clean my room',
       description: 'Clean my room and make it tidy',
       completed: true,
@@ -60,16 +63,16 @@ const TaskList: React.FC = () => {
       <h2 className="text-cyan-600 font-bold text-4xl text-center">Task List</h2>
       <ul className="py-[2rem] text-gray-300">
         {tasks.map(task => (
-          <li key={task.id} className="bg-slate-500 rounded-md m-[2rem] py-2 px-4 flex items-center">
+          <li key={task.id} className="bg-slate-900 rounded-md m-[2rem] py-2 px-4 flex items-center">
             <div className={`h-4 w-4 rounded-full ${getDotColor(task.status)} mr-2`} />
             <div className="flex-1">
-              <h3 className="text-xl font-bold">{task.name}</h3>
+              <h3 className="text-xl font-bold mt-3 mb-2">{task.name}</h3>
               <p>{task.description}</p>
             </div>
             <select
               value={task.status}
               onChange={(e) => handleStatusChange(task.id, e.target.value)}
-              className="px-4 py-2 rounded bg-gray-300 text-gray-700"
+              className="px-4 py-2 bg-cyan-600 text-white rounded-full"
             >
               <option value="Completed">Completed</option>
               <option value="Pending">Pending</option>
