ae723b898f46d63ae6c6c5264b1342037961dc1d
NurkaAmre <nurkerey30@gmail.com>
2023-12-14 09:23:28 +0600

    Implement task reducer

---
+++ src/redux/reducers.ts
@@ -0,0 +1,29 @@
+// reducers.ts
+
+import * as actionTypes from '../types/actionTypes';
+import { TaskState } from '../types/taskTypes';
+
+const initialState: TaskState = {
+  tasks: [],
+};
+
+const taskReducer = (state = initialState, action: actionTypes.TaskActionTypes): TaskState => {
+  switch (action.type) {
+    case actionTypes.ADD_TASK:
+      return {
+        ...state,
+        tasks: [...state.tasks, action.payload],
+      };
+    case actionTypes.UPDATE_TASK_STATUS:
+      return {
+        ...state,
+        tasks: state.tasks.map((task) =>
+          task.id === action.payload.taskId ? { ...task, status: action.payload.selectedStatus } : task
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default taskReducer;
